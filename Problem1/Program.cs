c267e05b8d039a6812305fcd2f1ed6fa40fdcefd
nikil <nikil@DESKTOP-73MHGNO>
2021-12-30 16:31:37 -0500

    Add project files.

---
+++ Problem1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalSum = 0;
+
+            for(int i = 5; i <1000; i = i + 5)
+            {
+                totalSum = totalSum + i;
+            }
+
+            for (int j = 3; j < 1000; j = j + 3)
+            {
+                if(j%5 != 0)
+                    totalSum = totalSum + j;
+            }
+
+            Console.WriteLine("Result is: " + totalSum);
+        }
+    }
+}
